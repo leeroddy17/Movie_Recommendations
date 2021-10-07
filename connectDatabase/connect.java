a92d93308cd9256b0a0f1764dd3f9037ad2288fa
sriram-mittal <sriram.mittal@tamu.edu>
2021-10-07 13:57:08 -0500

    added code to connect to reference later

--- connectDatabase/connect.java
+++ connectDatabase/connect.java
@@ -30,3 +30,34 @@ public class connect {
         // System.out.println("Opened database successfully");
     }
 }//end Class
+
+// String cus_lname = "";
+        // try{
+        //     //create a statement object
+        //     Statement stmt = db.conn.createStatement();
+        //     //create an SQL statement
+        //     String sqlStatement = "SELECT rating FROM customer_ratings LIMIT 10";
+        //     //send statement to DBMS
+        //     ResultSet result = stmt.executeQuery(sqlStatement);
+    
+        //     //OUTPUT
+        //     JOptionPane.showMessageDialog(null,"Customer rating from the Database.");
+        //     //System.out.println("______________________________________");
+        //     while (result.next()) {
+        //         //System.out.println(result.getString("cus_lname"));
+        //         cus_lname += result.getString("rating")+"\n";
+        //     }
+        // } catch (Exception e){
+        //     JOptionPane.showMessageDialog(null,e.getClass().getName()+": "+e.getMessage());
+        // }
+        // JOptionPane.showMessageDialog(null,cus_lname);
+        //closing the connection
+
+            // contentView.setVisible(true);
+        
+        // try {
+        //     db.conn.close();
+        //     JOptionPane.showMessageDialog(null,"Connection Closed.");
+        // } catch(Exception e) {
+        //     JOptionPane.showMessageDialog(null,"Connection NOT Closed.");
+        // }//end try catch
