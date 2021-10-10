7c7bd5beddb0009c616e93d26fdab90200c92122
Tombo-Combo77 <tcousins00@gmail.com>
2021-10-10 14:31:53 -0500

    Set up GUI for SQL connection

--- AnalysisView.java
+++ AnalysisView.java
@@ -4,110 +4,126 @@ import javax.swing.*;
 import java.awt.event.*;
 import java.awt.*;
 
-
 public class AnalysisView
 {
 	public static void main(String[] args) // maybe pass the strings of the movies as arguments here
 	{
 		JFrame analysistView = new JFrame();
-		JLabel userTitle = new JLabel(); // a panel for a title
-		JLabel TITLE = new JLabel(); // main title that should never be touched ever again
+		// JLabel userTitle = new JLabel(); // a panel for a title
+		// JLabel TITLE = new JLabel(); // main title that should never be touched ever again
 		
-		JCheckBox horror = new JCheckBox("Horror");
-		JCheckBox thriller = new JCheckBox("Thriller");
-		JCheckBox action = new JCheckBox("Action");
-		JCheckBox adventure = new JCheckBox("Adventure");
-		JCheckBox romance = new JCheckBox("Romance");
-		JCheckBox comedy = new JCheckBox("Comedy");
-		JCheckBox sciFi = new JCheckBox("Sci-Fi");
-		JCheckBox documentary = new JCheckBox("Documentary");
-		JCheckBox mystery = new JCheckBox("Mystery");
+		// JCheckBox horror = new JCheckBox("Horror");
+		// JCheckBox thriller = new JCheckBox("Thriller");
+		// JCheckBox action = new JCheckBox("Action");
+		// JCheckBox adventure = new JCheckBox("Adventure");
+		// JCheckBox romance = new JCheckBox("Romance");
+		// JCheckBox comedy = new JCheckBox("Comedy");
+		// JCheckBox sciFi = new JCheckBox("Sci-Fi");
+		// JCheckBox documentary = new JCheckBox("Documentary");
+		// JCheckBox mystery = new JCheckBox("Mystery");
 		
 		
-		JLabel boxTitle = new JLabel("Filter By Genre:");
-		boxTitle.setBounds(100, 125, 100, 100);
-		analysistView.add(boxTitle);
+		// JLabel boxTitle = new JLabel("Filter By Genre:");
+		// boxTitle.setBounds(100, 125, 100, 100);
+		// analysistView.add(boxTitle);
 		
-		Box CheckBoxes = Box.createVerticalBox();
-		CheckBoxes.setBounds(100, 200, 100, 300);
-		CheckBoxes.add(horror);
-		CheckBoxes.add(thriller);
-		CheckBoxes.add(action);
-		CheckBoxes.add(adventure);
-		CheckBoxes.add(romance);
-		CheckBoxes.add(comedy);
-		CheckBoxes.add(sciFi);
-		CheckBoxes.add(documentary);
-		CheckBoxes.add(mystery);
-		analysistView.add(CheckBoxes);
+		// Box CheckBoxes = Box.createVerticalBox();
+		// CheckBoxes.setBounds(100, 200, 100, 300);
+		// CheckBoxes.add(horror);
+		// CheckBoxes.add(thriller);
+		// CheckBoxes.add(action);
+		// CheckBoxes.add(adventure);
+		// CheckBoxes.add(romance);
+		// CheckBoxes.add(comedy);
+		// CheckBoxes.add(sciFi);
+		// CheckBoxes.add(documentary);
+		// CheckBoxes.add(mystery);
+		// analysistView.add(CheckBoxes);
 		
 		
 
-		JCheckBox studio1 = new JCheckBox("Warner Bros");
-		JCheckBox studio2 = new JCheckBox("Universal");
-		JCheckBox studio3 = new JCheckBox("Marvel");
-		JCheckBox studio4 = new JCheckBox("DreamWorks");
-		JCheckBox studio5 = new JCheckBox("Sony");
-		JCheckBox studio6 = new JCheckBox("Paramount");
-		JLabel studioTitle = new JLabel("Filter By Studio:");
-		studioTitle.setBounds(500, 125, 100, 100);
-		analysistView.add(studioTitle);
+		// JCheckBox studio1 = new JCheckBox("Warner Bros");
+		// JCheckBox studio2 = new JCheckBox("Universal");
+		// JCheckBox studio3 = new JCheckBox("Marvel");
+		// JCheckBox studio4 = new JCheckBox("DreamWorks");
+		// JCheckBox studio5 = new JCheckBox("Sony");
+		// JCheckBox studio6 = new JCheckBox("Paramount");
+		// JLabel studioTitle = new JLabel("Filter By Studio:");
+		// studioTitle.setBounds(500, 125, 100, 100);
+		// analysistView.add(studioTitle);
 		
-		Box studioBoxes = Box.createVerticalBox();
-		studioBoxes.setBounds(500, 200, 100, 300);
-		studioBoxes.add(studio1);
-		studioBoxes.add(studio2);
-		studioBoxes.add(studio3);
-		studioBoxes.add(studio4);
-		studioBoxes.add(studio5);
-		studioBoxes.add(studio6);
+		// Box studioBoxes = Box.createVerticalBox();
+		// studioBoxes.setBounds(500, 200, 100, 300);
+		// studioBoxes.add(studio1);
+		// studioBoxes.add(studio2);
+		// studioBoxes.add(studio3);
+		// studioBoxes.add(studio4);
+		// studioBoxes.add(studio5);
+		// studioBoxes.add(studio6);
 		
-		analysistView.add(studioBoxes);
+		// analysistView.add(studioBoxes);
 		
-		
-		
-		
-		
-		String UserName = "Carlos"; // get the users name and put it here later
+		// String UserName = "Carlos"; // get the users name and put it here later
 		
 		
-		userTitle.setBounds(100,-100, 1000, 300);
-		userTitle.setFont(new Font("Times New Roman", Font.PLAIN, 50));
-		userTitle.setText("Welcome Back " + UserName + "!");
-		analysistView.add(userTitle); // Add the name of the user to the 
+		// userTitle.setBounds(100,-100, 1000, 300);
+		// userTitle.setFont(new Font("Times New Roman", Font.PLAIN, 50));
+		// userTitle.setText("Welcome Back " + UserName + "!");
+		// analysistView.add(userTitle); // Add the name of the user to the 
 		
-		TITLE.setBounds(100, -200, 1000, 300);
-		TITLE.setFont(new Font("Times New Roman", Font.PLAIN, 50));
-		TITLE.setText("Aggie Movies For Researchers");
+		// TITLE.setBounds(100, -200, 1000, 300);
+		// TITLE.setFont(new Font("Times New Roman", Font.PLAIN, 50));
+		// TITLE.setText("Aggie Movies For Researchers");
 		
 		
 		
-		String movie1 = "avengers"; // name of the 3 most popular or relevant movies
-		String movie2 = "inception";
-		String movie3 = "Fast & Furious 6";
+		// String movie1 = "avengers"; // name of the 3 most popular or relevant movies
+		// String movie2 = "inception";
+		// String movie3 = "Fast & Furious 6";
 		  
-		// Buttons for the user to click on the Featured movies
-        JButton button1 = new JButton(movie1); //"place holder should be changed to movie name
-        JButton button2 = new JButton(movie2);
-        JButton button3 = new JButton(movie3);
-  
-        // x axis, y axis, width, height
-        button1.setBounds(100, 500, 300, 200);
-        button2.setBounds(500, 500, 300, 200);
-        button3.setBounds(900, 500, 300, 200);
-  
-        analysistView.add(button1); // buttons added to the frame
-        analysistView.add(button2);
-        analysistView.add(button3);
+		// // Buttons for the user to click on the Featured movies
+        // JButton button1 = new JButton(movie1); //"place holder should be changed to movie name
+        // JButton button2 = new JButton(movie2);
+        // JButton button3 = new JButton(movie3);
         
-       
+        JLabel dateRangeLabel = new JLabel();
+        dateRangeLabel.setText("Top Ten Movies From (mm/dd/yyyy): ");
+        dateRangeLabel.setBounds(100, 100, 300, 30);
+        analysistView.add(dateRangeLabel);
         
-          
+        JTextField fromDate = new JTextField(10);
+        fromDate.setBounds(350, 100, 100, 30);
+        analysistView.add(fromDate);
+
+        JLabel dateRangeLabel2 = new JLabel();
+        dateRangeLabel2.setText("To (mm/dd/yyyy): ");
+        dateRangeLabel2.setBounds(470, 100, 300, 30);
+        analysistView.add(dateRangeLabel2);
+
+        JTextField toDate = new JTextField(10);
+        toDate.setBounds(650, 100, 100, 30);
+        analysistView.add(toDate);
+
+        JButton calculate = new JButton("Calculate");
+        calculate.setBounds(750, 100, 100, 30);
+        analysistView.add(calculate);
+
+
+
+        JButton[] TopMovies = new JButton[10];
+        for (int i=0; i<5; i++){
+            TopMovies[i] = new JButton("");
+            TopMovies[i].setBounds(100+ 80*i, 150, 75, 60);
+            analysistView.add(TopMovies[i]);
+        } 
+        for (int i=5; i<10; i++){
+            TopMovies[i] = new JButton("");
+            TopMovies[i].setBounds(100+ 80*(i-5), 230, 75, 60);
+            analysistView.add(TopMovies[i]);
+        }
+                  
         analysistView.setSize(10000, 10000); // initial size of the frame can still be changed by the user
         analysistView.setLayout(null);
         analysistView.setVisible(true);
-        ////////////////////////////////////////////////////////////////////////////
-		/////////ABOVE IS ANALYST VIEWER //////////////////////////////////////////
-        
 	}
 }
--- contentView.java
+++ contentView.java
@@ -10,68 +10,109 @@ public class contentView
 	{
 		
 		JFrame contentView = new JFrame();
-		JLabel userTitle = new JLabel(); // a panel for a title
-		JLabel TITLE = new JLabel(); // main title that should never be touched ever again
+		// JLabel userTitle = new JLabel(); // a panel for a title
+		// JLabel TITLE = new JLabel(); // main title that should never be touched ever again
 		
 		
-		String UserName = "Carlos"; // get the users name and put it here later
+		// String UserName = "Carlos"; // get the users name and put it here later
 		
 		
-		userTitle.setBounds(100,-100, 1000, 300);
-		userTitle.setFont(new Font("Times New Roman", Font.PLAIN, 50));
-		userTitle.setText("Welcome Back " + UserName + "!");
-		contentView.add(userTitle); // Add the name of the user to the 
+		// userTitle.setBounds(100,-100, 1000, 300);
+		// userTitle.setFont(new Font("Times New Roman", Font.PLAIN, 50));
+		// userTitle.setText("Welcome Back " + UserName + "!");
+		// contentView.add(userTitle); // Add the name of the user to the 
 		
-		TITLE.setBounds(100, -200, 1000, 300);
-		TITLE.setFont(new Font("Times New Roman", Font.PLAIN, 50));
-		TITLE.setText("Aggie Movies For Researchers");
+		// TITLE.setBounds(100, -200, 1000, 300);
+		// TITLE.setFont(new Font("Times New Roman", Font.PLAIN, 50));
+		// TITLE.setText("Aggie Movies For Researchers");
 		
 		
 		
-		String movie1 = "avengers"; // name of the 3 most popular or relevant movies
-		String movie2 = "inception";
-		String movie3 = "Fast & Furious 6";
+		// String movie1 = "avengers"; // name of the 3 most popular or relevant movies
+		// String movie2 = "inception";
+		// String movie3 = "Fast & Furious 6";
 		
-		String movie4 = "Interstellar"; // 3 movies based on what was recently watched
-		String movie5 = "Tenet";
-		String movie6 = "The Dark Knight";
+		// String movie4 = "Interstellar"; // 3 movies based on what was recently watched
+		// String movie5 = "Tenet";
+		// String movie6 = "The Dark Knight";
 		
-		// Buttons for the user to click on the Featured movies
-		JLabel recomended = new JLabel("Recomended for you");
-		recomended.setBounds(100, 350, 1000, 200);
-		recomended.setFont(new Font("Times New Roman", Font.PLAIN, 50));
-		contentView.add(recomended);
+		// // Buttons for the user to click on the Featured movies
+		// JLabel recomended = new JLabel("Recomended for you");
+		// recomended.setBounds(100, 350, 1000, 200);
+		// recomended.setFont(new Font("Times New Roman", Font.PLAIN, 50));
+		// contentView.add(recomended);
 		
-        JButton button1 = new JButton(movie1); //"place holder should be changed to movie name
-        JButton button2 = new JButton(movie2);
-        JButton button3 = new JButton(movie3);
+        // JButton button1 = new JButton(movie1); //"place holder should be changed to movie name
+        // JButton button2 = new JButton(movie2);
+        // JButton button3 = new JButton(movie3);
         
-        JLabel watched = new JLabel("Becasue you watched <place holder>");
-        watched.setBounds(100, 50, 1000, 200);
-        watched.setFont(new Font("Times New Roman", Font.PLAIN, 50));
-        contentView.add(watched);
+        // JLabel watched = new JLabel("Becasue you watched <place holder>");
+        // watched.setBounds(100, 50, 1000, 200);
+        // watched.setFont(new Font("Times New Roman", Font.PLAIN, 50));
+        // contentView.add(watched);
         
-        JButton button4 = new JButton(movie4); //place holder should be changed to movie name through database data
-        JButton button5 = new JButton(movie5);
-        JButton button6 = new JButton(movie6);
+        // JButton button4 = new JButton(movie4); //place holder should be changed to movie name through database data
+        // JButton button5 = new JButton(movie5);
+        // JButton button6 = new JButton(movie6);
   
-        // x axis, y axis, width, height for location on the page
-        button1.setBounds(100, 500, 300, 200);
-        button2.setBounds(500, 500, 300, 200);
-        button3.setBounds(900, 500, 300, 200);
-        button4.setBounds(100, 200, 300, 200);
-        button5.setBounds(500, 200, 300, 200);
-        button6.setBounds(900, 200, 300, 200);
+        // // x axis, y axis, width, height for location on the page
+        // button1.setBounds(100, 500, 300, 200);
+        // button2.setBounds(500, 500, 300, 200);
+        // button3.setBounds(900, 500, 300, 200);
+        // button4.setBounds(100, 200, 300, 200);
+        // button5.setBounds(500, 200, 300, 200);
+        // button6.setBounds(900, 200, 300, 200);
   
-        contentView.add(button1); // buttons added to the frame
-        contentView.add(button2);
-        contentView.add(button3);
-        contentView.add(button4); 
-        contentView.add(button5);
-        contentView.add(button6);
+        // contentView.add(button1); // buttons added to the frame
+        // contentView.add(button2);
+        // contentView.add(button3);
+        // contentView.add(button4); 
+        // contentView.add(button5);
+        // contentView.add(button6);
         
-       
+        JLabel uidLabel = new JLabel();
+        uidLabel.setText("User ID:");
+        uidLabel.setBounds(10, 25, 50, 30);
+        contentView.add(uidLabel);
         
+        JTextField uid = new JTextField(10);
+        uid.setBounds(65, 25, 100, 30);
+        contentView.add(uid);
+        contentView.add(uid);
+
+        JLabel dateRangeLabel = new JLabel();
+        dateRangeLabel.setText("Watch History From (mm/dd/yyyy): ");
+        dateRangeLabel.setBounds(100, 100, 300, 30);
+        contentView.add(dateRangeLabel);
+        
+        JTextField fromDate = new JTextField(10);
+        fromDate.setBounds(350, 100, 100, 30);
+        contentView.add(fromDate);
+
+        JLabel dateRangeLabel2 = new JLabel();
+        dateRangeLabel2.setText("To (mm/dd/yyyy): ");
+        dateRangeLabel2.setBounds(470, 100, 300, 30);
+        contentView.add(dateRangeLabel2);
+
+        JTextField toDate = new JTextField(10);
+        toDate.setBounds(650, 100, 100, 30);
+        contentView.add(toDate);
+
+        JButton calculate = new JButton("Calculate");
+        calculate.setBounds(750, 100, 100, 30);
+        contentView.add(calculate);
+        
+        JButton[] TopMovies = new JButton[10];
+        for (int i=0; i<5; i++){
+            TopMovies[i] = new JButton("");
+            TopMovies[i].setBounds(100+ 80*i, 150, 75, 60);
+            contentView.add(TopMovies[i]);
+        } 
+        for (int i=5; i<10; i++){
+            TopMovies[i] = new JButton("");
+            TopMovies[i].setBounds(100+ 80*(i-5), 230, 75, 60);
+            contentView.add(TopMovies[i]);
+        }
           
         contentView.setSize(10000, 10000); // initial size of the frame can still be changed by the user
         contentView.setLayout(null);
